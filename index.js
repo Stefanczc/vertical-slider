fa90c81405ed8a437f75aedb183a410cacabd361
Stefan Alexandru <33218445+Stefanczc@users.noreply.github.com>
2023-08-16 00:18:35 +0300

    update index js

--- index.js
+++ index.js
@@ -3,26 +3,46 @@ const rightBtn = document.getElementById('right-btn');
 const leftItems = document.querySelectorAll('.left-side-container div');
 const rightItems = document.querySelectorAll('.right-side-container div');
 
-for (let i = 0; i < leftItems.length; i++) {
-    let leftItem = leftItems[i];
-    let rightItem = rightItems[i];
-    let viewHeight = 100;
-    
-    leftBtn.addEventListener('click', () => {
+let viewHeight = 100;
+let rightHeight = 100;
+
+leftBtn.addEventListener('click', () => {
+    for (let i = 0; i < leftItems.length; i++) {
         if (viewHeight === 400) {
             viewHeight = 0;
-        } 
+        }
+        if(rightHeight === 400) {
+            rightHeight = 0;
+        }
+        let leftItem = leftItems[i];
+        let rightItem = rightItems[i];
         leftItem.style.transform = `translate(0, -${viewHeight}vh)`;
-        rightItems[i].style.transform = `translate(0, -${300 - viewHeight}vh)`;
-        viewHeight += 100;
-        console.log(viewHeight);
-    })
-    rightBtn.addEventListener('click', () => { 
-        if (viewHeight === 400) {
-            viewHeight = 0;
-        } 
-        leftItem.style.transform = `translate(0, -${viewHeight}vh)`;
-        rightItem.style.transform = `translate(0, -${300 - viewHeight}vh)`;
-        viewHeight += 100;
-    });
-}
+        rightItem.style.transform = `translate(0, -${300 - rightHeight}vh)`;
+    }
+    viewHeight += 100;
+    rightHeight += 100;
+});
+
+rightBtn.addEventListener('click', () => {
+    if (viewHeight === 100) {
+        viewHeight = 500;
+    }
+    if (rightHeight === 100) {
+        rightHeight = 500;
+    }
+    for (let i = 0; i < rightItems.length; i++) {
+        let leftItem = leftItems[i];
+        let rightItem = rightItems[i];
+        leftItem.style.transform = `translate(0, -${viewHeight - 200}vh)`;
+        rightItem.style.transform = `translate(0, -${500 - rightHeight}vh)`;
+    }
+    viewHeight -= 100;
+    rightHeight -= 100;
+
+})
+
+
+
+
+
+
